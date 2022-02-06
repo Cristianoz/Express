95779389dc62cd40d27391a3a0acc6faf1040c6a
Cristianoz <mayteoelan@gmail.com>
2022-02-06 18:41:01 -0500

    servidor local

--- src/index.js
+++ src/index.js
@@ -1,3 +1,3 @@
 const app = require('./app');
 const port = process.env.PORT || 3040
-app.listen(port, ()=>{console.log(`server is running on this ${port}`)}); // 5. creacion del servidor local
+app.listen(port, ()=>{console.log(`server is running on this ${port}`)}); // 5. creacion del servidor locales
